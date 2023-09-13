c2d11e6dd122753028355f1a48746ab660de16a2
zxchen98 <43800081+zxchen98@users.noreply.github.com>
2023-09-12 20:45:33 -0700

    update celery

--- dummy_api/celery.py
+++ dummy_api/celery.py
@@ -3,11 +3,10 @@ import os
 from celery import Celery, current_app
 from django.conf import settings
 from celery.schedules import crontab
-from api.src.tasks import daily_billing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BACKEND_API.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dummy_api.settings')
 
-app = Celery('BACKEND_API')
+app = Celery('dummy_api')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
@@ -16,6 +15,7 @@ app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 @current_app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     # Runs the daily_billing task every day at midnight
+    from api.src.tasks import daily_billing
     sender.add_periodic_task(
         crontab(hour=0, minute=0),
         daily_billing.s(),
