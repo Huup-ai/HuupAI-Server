b4d7c1b4362936bf84d4243d1ea2dfecf75baabf
zxchen98 <43800081+zxchen98@users.noreply.github.com>
2023-09-18 10:56:05 -0700

    allow credentials

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, Instance, Invoice, Pricing
+from .models import *
 
 admin.site.register(User)
 admin.site.register(Instance)
 admin.site.register(Invoice)
 admin.site.register(Pricing)
+admin.site.register(Wallet)
