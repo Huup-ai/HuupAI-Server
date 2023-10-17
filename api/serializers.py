b6e5e32879f5ae10b4e0e966094edcad8a072a2d
zxchen98 <43800081+zxchen98@users.noreply.github.com>
2023-10-16 21:06:03 -0700

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -9,7 +9,7 @@ class InstanceSerializer(serializers.ModelSerializer):
         model = Instance
         fields = [
             'instance_id','cluster', 'vm_name',
-            'status', 'usage', 'price', 'payment'
+            'status', 'usage', 'price', 'payment_method'
         ]
         depth = 1
 
