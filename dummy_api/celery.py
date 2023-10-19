0c0ea9610b68ac336c9ce2b3bbe565a2e35f827d
zxchen98 <43800081+zxchen98@users.noreply.github.com>
2023-10-18 17:06:27 -0700

    celery

--- dummy_api/celery.py
+++ dummy_api/celery.py
@@ -1,26 +1,26 @@
-from __future__ import absolute_import, unicode_literals
-import os
-from celery import Celery, current_app
-from django.conf import settings
-from celery.schedules import crontab
+# from __future__ import absolute_import, unicode_literals
+# import os
+# from celery import Celery, current_app
+# from django.conf import settings
+# from celery.schedules import crontab
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dummy_api.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dummy_api.settings')
 
-app = Celery('dummy_api')
+# app = Celery('dummy_api')
 
-app.config_from_object('django.conf:settings', namespace='CELERY')
+# app.config_from_object('django.conf:settings', namespace='CELERY')
 
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+# app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
-@current_app.on_after_configure.connect
-def setup_periodic_tasks(sender, **kwargs):
-    # Runs the daily_billing task every day at midnight
-    from api.tasks import daily_billing_task
-    sender.add_periodic_task(
-        crontab(hour=0, minute=0),
-        daily_billing_task.s(),
-    )
+# @current_app.on_after_configure.connect
+# def setup_periodic_tasks(sender, **kwargs):
+#     # Runs the daily_billing task every day at midnight
+#     from api.tasks import daily_billing_task
+#     sender.add_periodic_task(
+#         crontab(hour=0, minute=0),
+#         daily_billing_task.s(),
+#     )
 
-    from api.tasks import check_instance_status
-    # Runs the check_instance_status every hour
-    sender.add_periodic_task(3600.0, check_instance_status.s(), name='Check instance status every hour')
+#     from api.tasks import check_instance_status
+#     # Runs the check_instance_status every hour
+#     sender.add_periodic_task(3600.0, check_instance_status.s(), name='Check instance status every hour')
--- dummy_api/settings.py
+++ dummy_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from celery.schedules import crontab
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -47,6 +48,13 @@ CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'UTC'
 
+CELERY_BEAT_SCHEDULE = {
+    'my-hourly-task': {
+        'task': 'api.tasks.check_instance_status',
+        'schedule': crontab(minute=0, hour='*'),
+    },
+}
+
 #Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.zoho.com'
@@ -73,6 +81,7 @@ INSTALLED_APPS = [
     'api',
     'corsheaders',
     'drf_yasg',
+    'django_celery_beat',
 ]
 AUTH_USER_MODEL = 'api.User'
 
