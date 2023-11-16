2c4fc1c3aa8395a6dd3404d27b690ac67fc508a0
zxchen98 <43800081+zxchen98@users.noreply.github.com>
2023-11-16 11:22:38 -0800

    Update views.py

--- api/views.py
+++ api/views.py
@@ -492,7 +492,7 @@ class UserRegistrationAPI(APIView):
 class GoogleLoginView(APIView):
     permission_classes = (permissions.AllowAny,)
     def post(self, request, *args, **kwargs):
-        token = request.data.get('token_id')
+        token = request.data.get('token')
         try:
             # Validate the token
             idinfo = id_token.verify_oauth2_token(token, transport.requests.Request(), settings.GOOGLE_OAUTH2_CLIENT_ID)
